4e55e543001d5d9fc51b4a1359e7a331f67341cc
Márk Bartha <mark.bartha@lombiq.com>
2015-09-08 16:06:21 +0200

    Removing unnecessary code and files.
    --HG--
    branch : dev

--- Lombiq.VisualStudioExtensions/PkgCmdID.cs
+++ Lombiq.VisualStudioExtensions/PkgCmdID.cs
@@ -6,7 +6,5 @@ namespace Lombiq.VisualStudioExtensions
     static class PkgCmdIDList
     {
         public const uint cmdidInjectDependency = 0x100;
-        public const uint cmdidAddModule = 0x101;
-        public const uint cmdidTest = 0x102;
     };
 }
