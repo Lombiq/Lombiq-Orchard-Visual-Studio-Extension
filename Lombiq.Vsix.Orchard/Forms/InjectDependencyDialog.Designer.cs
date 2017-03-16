0dfbb8e8f10e6fda331e8b67d95c0a3b86e6dd01
Márk Bartha <mark.bartha@lombiq.com>
2017-03-16 10:58:59 +0100

    Renaming default component names
    --HG--
    branch : issue/VSIX-15

--- Lombiq.Vsix.Orchard/Forms/InjectDependencyDialog.Designer.cs
+++ Lombiq.Vsix.Orchard/Forms/InjectDependencyDialog.Designer.cs
@@ -28,45 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.dependencyLabel = new System.Windows.Forms.Label();
             this.dependencyNameTextBox = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.separatorLabel = new System.Windows.Forms.Label();
             this.fieldNameTextBox = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.fieldNameLabel = new System.Windows.Forms.Label();
             this.generateShortFieldNameCheckBox = new System.Windows.Forms.CheckBox();
-            this.label6 = new System.Windows.Forms.Label();
+            this.hintLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // okButton
             // 
-            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button1.Location = new System.Drawing.Point(193, 154);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "OK";
-            this.button1.UseVisualStyleBackColor = true;
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(193, 154);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 0;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // cancelButton
             // 
-            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button2.Location = new System.Drawing.Point(274, 154);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(274, 154);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // dependencyLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Dependency:";
+            this.dependencyLabel.AutoSize = true;
+            this.dependencyLabel.Location = new System.Drawing.Point(12, 52);
+            this.dependencyLabel.Name = "dependencyLabel";
+            this.dependencyLabel.Size = new System.Drawing.Size(71, 13);
+            this.dependencyLabel.TabIndex = 2;
+            this.dependencyLabel.Text = "Dependency:";
             // 
             // dependencyNameTextBox
             // 
@@ -76,14 +76,14 @@
             this.dependencyNameTextBox.TabIndex = 3;
             this.dependencyNameTextBox.TextChanged += new System.EventHandler(this.dependencyNameTextBox_TextChanged);
             // 
-            // label2
+            // separatorLabel
             // 
-            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label2.Location = new System.Drawing.Point(-1, 136);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(362, 2);
-            this.label2.TabIndex = 4;
-            this.label2.Text = " ";
+            this.separatorLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.separatorLabel.Location = new System.Drawing.Point(-1, 136);
+            this.separatorLabel.Name = "separatorLabel";
+            this.separatorLabel.Size = new System.Drawing.Size(362, 2);
+            this.separatorLabel.TabIndex = 4;
+            this.separatorLabel.Text = " ";
             // 
             // fieldNameTextBox
             // 
@@ -92,14 +92,14 @@
             this.fieldNameTextBox.Size = new System.Drawing.Size(260, 20);
             this.fieldNameTextBox.TabIndex = 6;
             // 
-            // label3
+            // fieldNameLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 76);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(61, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Field name:";
+            this.fieldNameLabel.AutoSize = true;
+            this.fieldNameLabel.Location = new System.Drawing.Point(12, 76);
+            this.fieldNameLabel.Name = "fieldNameLabel";
+            this.fieldNameLabel.Size = new System.Drawing.Size(61, 13);
+            this.fieldNameLabel.TabIndex = 5;
+            this.fieldNameLabel.Text = "Field name:";
             // 
             // generateShortFieldNameCheckBox
             // 
@@ -112,31 +112,31 @@
             this.generateShortFieldNameCheckBox.UseVisualStyleBackColor = true;
             this.generateShortFieldNameCheckBox.CheckedChanged += new System.EventHandler(this.dependencyNameTextBox_TextChanged);
             // 
-            // label6
+            // hintLabel
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 12);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(257, 13);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "Give the name of the dependency you want to inject.";
+            this.hintLabel.AutoSize = true;
+            this.hintLabel.Location = new System.Drawing.Point(12, 12);
+            this.hintLabel.Name = "hintLabel";
+            this.hintLabel.Size = new System.Drawing.Size(257, 13);
+            this.hintLabel.TabIndex = 12;
+            this.hintLabel.Text = "Give the name of the dependency you want to inject.";
             // 
             // InjectDependencyDialog
             // 
-            this.AcceptButton = this.button1;
+            this.AcceptButton = this.okButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.button2;
+            this.CancelButton = this.cancelButton;
             this.ClientSize = new System.Drawing.Size(361, 189);
-            this.Controls.Add(this.label6);
+            this.Controls.Add(this.hintLabel);
             this.Controls.Add(this.generateShortFieldNameCheckBox);
             this.Controls.Add(this.fieldNameTextBox);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fieldNameLabel);
+            this.Controls.Add(this.separatorLabel);
             this.Controls.Add(this.dependencyNameTextBox);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dependencyLabel);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "InjectDependencyDialog";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
@@ -148,14 +148,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label dependencyLabel;
         private System.Windows.Forms.TextBox dependencyNameTextBox;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label separatorLabel;
         private System.Windows.Forms.TextBox fieldNameTextBox;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label fieldNameLabel;
         private System.Windows.Forms.CheckBox generateShortFieldNameCheckBox;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label hintLabel;
     }
 }
