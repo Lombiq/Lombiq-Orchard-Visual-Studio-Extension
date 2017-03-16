6a4ded1798cf70eed6876e086554cae20f66d934
Márk Bartha <mark.bartha@lombiq.com>
2017-03-16 10:59:53 +0100

    Code styling in DefaultFieldNameFromGenericTypeGenerator.cs
    --HG--
    branch : issue/VSIX-15

--- Lombiq.Vsix.Orchard/Services/DefaultFieldNameFromGenericTypeGenerator.cs
+++ Lombiq.Vsix.Orchard/Services/DefaultFieldNameFromGenericTypeGenerator.cs
@@ -10,7 +10,10 @@ namespace Lombiq.Vsix.Orchard.Services
         public override double Priority { get { return 10; } }
 
 
-        public override bool CanGenerate(string dependency) { return Regex.IsMatch(dependency, GenericTypeNameRegexPattern); }
+        public override bool CanGenerate(string dependency)
+        {
+            return Regex.IsMatch(dependency, GenericTypeNameRegexPattern);
+        }
 
         public override string Generate(string dependency, bool useShortName)
         {
@@ -18,8 +21,8 @@ namespace Lombiq.Vsix.Orchard.Services
             // after the underscore and the generic type to the end.
             var segments = GetGenericTypeSegments(dependency);
 
-            return useShortName ? 
-                    GetShortNameWithUnderscore(segments.CleanedGenericParameterName) + GetShortName(segments.CleanedGenericTypeName) : 
+            return useShortName ?
+                    GetShortNameWithUnderscore(segments.CleanedGenericParameterName) + GetShortName(segments.CleanedGenericTypeName) :
                     GetStringWithUnderscore(GetCamelCased(segments.CleanedGenericParameterName)) + segments.CleanedGenericTypeName;
         }
     }
