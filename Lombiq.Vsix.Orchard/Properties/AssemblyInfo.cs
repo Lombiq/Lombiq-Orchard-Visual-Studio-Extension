4ffbbbb2a00bd8bdb47b99d1776cdedc2ab95193
Csaba Boros <csaba.boros@lombiq.com>
2021-11-09 20:07:06 +0100

    Com Visibility

--- Lombiq.Vsix.Orchard/Properties/AssemblyInfo.cs
+++ Lombiq.Vsix.Orchard/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 
