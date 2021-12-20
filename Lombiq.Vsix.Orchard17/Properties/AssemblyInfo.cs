9dc6e4a47cb41f9c9246eb89c3a91cc897374d21
Csaba Boros <csaba.boros@lombiq.com>
2021-12-20 23:46:51 +0100

    Fix AssemblyInfo

--- Lombiq.Vsix.Orchard17/Properties/AssemblyInfo.cs
+++ Lombiq.Vsix.Orchard17/Properties/AssemblyInfo.cs
@@ -1,22 +1,22 @@
+using Lombiq.Vsix.Orchard.Constants;
+using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Lombiq.Vsix.Orchard17")]
+// General Information about an assembly is controlled through the following set of attributes. Change these attribute
+// values to modify the information associated with an assembly.
+[assembly: AssemblyTitle("Lombiq Orchard Visual Studio Extension")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lombiq.Vsix.Orchard17")]
+[assembly: AssemblyCompany("Lombiq")]
+[assembly: AssemblyProduct("Lombiq Orchard Visual Studio Extension")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -25,8 +25,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// You can specify all the values or you can default the Revision and Build Numbers by using the '*' as shown below:
+
+[assembly: AssemblyVersion(ExtensionVersion.Current)]
+[assembly: AssemblyFileVersion(ExtensionVersion.Current)]
