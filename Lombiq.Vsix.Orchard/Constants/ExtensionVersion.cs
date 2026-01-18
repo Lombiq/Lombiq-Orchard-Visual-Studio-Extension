b2235b47a1c53d9b59eafad4bb2e8c2986fcdcd3
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2026-01-18 15:35:27 +0100

    Increasing version numbers for local debugging

--- Lombiq.Vsix.Orchard/Constants/ExtensionVersion.cs
+++ Lombiq.Vsix.Orchard/Constants/ExtensionVersion.cs
@@ -2,7 +2,10 @@ namespace Lombiq.Vsix.Orchard.Constants
 {
     internal static class ExtensionVersion
     {
-        // This needs to be named "Version" for vsix-version-stamp to work during GitHub Actions builds.
-        public const string Version = "1.7.0";
+        // This needs to be named "Version" for vsix-version-stamp to work during GitHub Actions builds. Using an
+        // unrealistically high version number to ensure it's always higher than any released version and thus we can
+        // run it from source without it conflicting with the installed version. Same as in
+        // source.extension.vsixmanifest.
+        public const string Version = "99.0.0";
     }
 }
